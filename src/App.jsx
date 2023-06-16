37db460dd7e28a054e5fb6eec8cbb57220195921
Enes Sirin <sefsirin55@gmail.com>
2023-06-16 20:55:28 +0300

    ana template oluşturuldu ve fiiler eklendi

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+function App() {
+  return (
+    <div className='bg-gray-600 w-full min-h-screen flex justify-center items-center px-6'>
+        <div className="w-[700px] bg-white text-black md:h-[400px] h-[600px] py-2">
+          <h1 className='text-center text-2xl select-none cursor-pointer'>Let's time for practice</h1>
+          
+        </div>
+    </div>
+  )
+}
+
+export default App
